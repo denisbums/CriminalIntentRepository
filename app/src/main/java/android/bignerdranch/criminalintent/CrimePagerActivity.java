f77b58fd10685af76a132a2bec09795ee1f24e6c
Denis Pimkin <pimkinden@gmail.com>
2019-02-17 13:33:02 +0300

    11 - level

--- app/src/main/java/android/bignerdranch/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimePagerActivity.java
@@ -21,7 +21,7 @@ public class CrimePagerActivity extends AppCompatActivity {
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimePagerActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
-        return intent;
+        return intent;//
     }
 
     @Override
